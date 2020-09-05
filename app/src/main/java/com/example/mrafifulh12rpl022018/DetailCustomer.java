d9088146a87674ab3acb5b8587f859b1db9f173b
RafifulHan <anang2018rplrus2@gmail.com>
2020-09-05 21:57:25 +0700

    bike booking

--- app/src/main/java/com/example/mrafifulh12rpl022018/DetailCustomer.java
+++ app/src/main/java/com/example/mrafifulh12rpl022018/DetailCustomer.java
@@ -2,7 +2,9 @@ package com.example.mrafifulh12rpl022018;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -19,22 +21,22 @@ public class DetailCustomer extends AppCompatActivity {
         setContentView(R.layout.activity_detail_customer);
 
         String id = getIntent().getStringExtra("id");
-        String valueId = id.toString();
+        final String valueId = id.toString();
 
         String nama = getIntent().getStringExtra("nama");
-        String valueNama = nama.toString();
+        final String valueNama = nama.toString();
 
         String nohp = getIntent().getStringExtra("nohp");
-        String valueNohp = nohp.toString();
+        final String valueNohp = nohp.toString();
 
         String noktp = getIntent().getStringExtra("noktp");
-        String valueNoktp = noktp.toString();
+        final String valueNoktp = noktp.toString();
 
         String alamat = getIntent().getStringExtra("alamat");
-        String valueAlamat = alamat.toString();
+        final String valueAlamat = alamat.toString();
 
         String email = getIntent().getStringExtra("email");
-        String valueEmail = email.toString();
+        final String valueEmail = email.toString();
 
 
         txtnama = (TextView) findViewById(R.id.nama);
@@ -51,6 +53,21 @@ public class DetailCustomer extends AppCompatActivity {
 
         txtnoktp = (TextView) findViewById(R.id.noktp);
         txtnoktp.setText(noktp);
+
+        objId = (Button) findViewById(R.id.objId);
+        objId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DetailCustomer.this, EditCustomer.class);
+                intent.putExtra("id", valueId);
+                intent.putExtra("nama", valueNama);
+                intent.putExtra("email", valueEmail);
+                intent.putExtra("alamat", valueAlamat);
+                intent.putExtra("nohp", valueNohp);
+                intent.putExtra("noktp", valueNoktp);
+                startActivity(intent);
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/example/mrafifulh12rpl022018/EditCustomer.java
@@ -0,0 +1,120 @@
+package com.example.mrafifulh12rpl022018;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class EditCustomer extends AppCompatActivity {
+
+    EditText txtid, txtnama, txtalamat, txtemail, txtnohp, txtnoktp;
+    Button btnUpdate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_customer);
+
+        String id = getIntent().getStringExtra("id");
+
+        String nama = getIntent().getStringExtra("nama");
+
+        String nohp = getIntent().getStringExtra("nohp");
+
+        String noktp = getIntent().getStringExtra("noktp");
+
+        String alamat = getIntent().getStringExtra("alamat");
+
+        String email = getIntent().getStringExtra("email");
+
+
+
+        txtid = (EditText) findViewById(R.id.id);
+        txtid.setText(id);
+
+        txtnama = (EditText) findViewById(R.id.nama);
+        txtnama.setText(nama);
+
+        txtemail = (EditText) findViewById(R.id.email);
+        txtemail.setText(email);
+
+        txtalamat = (EditText) findViewById(R.id.alamat);
+        txtalamat.setText(alamat);
+
+        txtnohp = (EditText) findViewById(R.id.nohp);
+        txtnohp.setText(nohp);
+
+        txtnoktp = (EditText) findViewById(R.id.noktp);
+        txtnoktp.setText(noktp);
+
+        btnUpdate = (Button) findViewById(R.id.UpdateData);
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText updtNama, updtEmail, updtAlamat, updtNohp, updtNoktp, updtId;
+
+                updtId = (EditText) findViewById(R.id.id);
+                updtNama = (EditText) findViewById(R.id.nama);
+                updtEmail = (EditText) findViewById(R.id.email);
+                updtAlamat = (EditText) findViewById(R.id.alamat);
+                updtNohp = (EditText) findViewById(R.id.nohp);
+                updtNoktp = (EditText) findViewById(R.id.noktp);
+
+                AndroidNetworking.post("http://192.168.1.7/bike_booking/update.php")
+                        .addBodyParameter("id", updtId.getText().toString())
+                        .addBodyParameter("nama", updtNama.getText().toString())
+                        .addBodyParameter("email", updtEmail.getText().toString())
+                        .addBodyParameter("alamat", updtAlamat.getText().toString())
+                        .addBodyParameter("nohp", updtNohp.getText().toString())
+                        .addBodyParameter("noktp", updtNoktp.getText().toString())
+                        .setTag("test")
+                        .setPriority(Priority.MEDIUM)
+                        .build()
+                        .getAsJSONObject(new JSONObjectRequestListener() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                // do anything with response
+                                try {
+                                    JSONObject hasil = response.getJSONObject("result");
+                                    Log.d("HANS", "url: " + hasil.toString());
+                                    Boolean respon= hasil.getBoolean("response");
+                                    if (respon){
+                                        Toast.makeText(EditCustomer.this, "Update Sukses", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(getApplicationContext(), ListData.class));
+                                    }else{
+                                        Toast.makeText(EditCustomer.this, "Update Gagal", Toast.LENGTH_SHORT).show();
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            @Override
+                            public void onError(ANError error) {
+                                // handle error
+                                Log.d("HANS", "onError: " + error.getErrorBody());
+                                Log.d("HANS", "onError: " + error.getLocalizedMessage());
+                                Log.d("HANS", "onError: " + error.getErrorDetail());
+                                Log.d("HANS", "onError: " + error.getResponse());
+                                Log.d("HANS", "onError: " + error.getErrorCode());
+                            }
+                        });
+
+            }
+        });
+    }
+}
